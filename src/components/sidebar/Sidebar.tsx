7247fc6cfccfa9d5b99c1bea94f5f26c8ab18930
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2021-11-08 22:10:35 +0800

    Styling improvements, mobile support

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import endpoints from "../../helpers/endpoints.json";
 export default function Sidebar() {
 	return (
-		<nav>
-			<SidebarHeader />
-			{Object.entries(endpoints).map((category) => <SidebarGroup key={category[0]} categoryName={category[0]} endpoints={Object.keys(category[1])} />)}
-		</nav>
+		<div className="preload">
+			<nav className="hidden">
+				<SidebarHeader />
+				{Object.entries(endpoints).map((category) => <SidebarGroup key={category[0]} categoryName={category[0]} endpoints={Object.keys(category[1])} />)}
+			</nav>
+			<button className="hamburger hamburger--emphatic unclicked" id="nav-button" type="button" onClick={navClick}>
+				<span className="hamburger-box">
+					<span className="hamburger-inner"></span>
+				</span>
+			</button>  
+		</div>
 	);
 }
 import "./Sidebar.scss";
+
+const sleep = (time:number) => new Promise((resolve) => setTimeout(resolve, time));
+
+async function navClick() {
+	document.querySelector("#nav-button").classList.toggle("is-active");
+	document.querySelector("#nav-button").classList.remove("unclicked");
+
+	const nav = document.querySelector("nav");
+	if (nav.classList.contains("hidden")) {
+		nav.classList.remove("hidden");
+		await sleep(1);
+		nav.classList.add("mobile-visible");
+	} else {
+		nav.classList.remove("mobile-visible");
+		setTimeout(() => nav.classList.add("hidden"), 200);
+	}
+
+	const main = document.querySelector("main");
+	if (main.classList.contains("mobile-invisible")) {
+		main.classList.remove("mobile-invisible");
+	} else {
+		setTimeout(() => main.classList.add("mobile-invisible"), 200);
+	}
+
+}
 
 function SidebarHeader() {
 	return (
